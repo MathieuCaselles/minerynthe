31d4bb554f46f038f2cdf37db6499c99760691a2
MathieuCaselles <mathieu.caselles@ynov.com>
2019-11-06 13:57:29 +0100

    Add files via upload

---
+++ src/main/java/fr/minerynthe/minerynthe/Main.java
@@ -0,0 +1,17 @@
+package fr.minerynthe.minerynthe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("test");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
