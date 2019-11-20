d9c54149c73ad2932ec3cc8120c6baaaf5279be7
Mathieu <mathieu.caselles@ynov.com>
2019-11-20 11:51:16 +0100

    détecttion block obsidienne dans un rayon

--- src/main/java/fr/minerynthe/minerynthe/command/YoloCommand.java
+++ src/main/java/fr/minerynthe/minerynthe/command/YoloCommand.java
@@ -2,6 +2,8 @@ package fr.minerynthe.minerynthe.command;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,8 +13,19 @@ public class YoloCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
-        Location twoBlocksAway = player.getLocation().add(player.getLocation().getDirection().multiply(2));
-        twoBlocksAway.getBlock().setType(Material.EMERALD_BLOCK);
+        Location loc = player.getLocation();
+        int radius = 32;
+        World world = loc.getWorld();
+        for (int x = -radius; x < radius; x++) {
+            for (int y = -radius; y < radius; y++) {
+                for (int z = -radius; z < radius; z++) {
+                    Block block = world.getBlockAt(loc.getBlockX() + x, loc.getBlockY() + y, loc.getBlockZ() + z);
+                    if (block.getType() == Material.OBSIDIAN) {
+                        block.setType(Material.EMERALD_BLOCK);
+                    }
+                }
+            }
+        }
 
 
         return false;
